f14a9711e40076ca629be107c945b8a2e6ff1267
Johansito <johanmunoz319@gmail.com>
2024-05-19 14:38:59 -0500

    styles-switch, gradientes

--- js/while.js
+++ js/while.js
@@ -198,7 +198,7 @@ let btnEjercicio06 = document.getElementById('button6')
 
 btnEjercicio06.addEventListener('click', function() {
     document.getElementById('float-window-6').classList.remove('inactive')
-});
+}); 
 
 // Cierra ventana
 let closeBtn6 = document.getElementById('close-btn6')
@@ -208,42 +208,179 @@ closeBtn6.addEventListener('click', function() {
     ventanaEjercicio6.classList.add('inactive')
 })
 
-// Generador de secuencia Fibonacci
-
 let inputNumero = document.getElementById('cantidadDigitos')
 let calcDigitos = document.getElementById('calcDigitos')
 let respuestaDigitos = document.getElementById('muestraDigitos')
 
 calcDigitos.addEventListener('click', function(event) {
     event.preventDefault()
-    respuestaFibonacci.textContent = ``
-    let cantidadFibonacci = inputNFibonacci.value
-    let num1 = 0
-    let num2 = 1
-    let num3
+    let digitosUser = inputNumero.value
+
     let i = 1
-    respuestaFibonacci.innerHTML += `${num1}, `
-    while(i < cantidadFibonacci) {
-        num3 = num1 + num2
-        num1 = num2
-        num2 = num3
-        respuestaFibonacci.innerHTML += `${num1}, `
-        i++
+    let numeroDigitos = 0
+
+    while(digitosUser > 0) {
+        digitosUser = Math.floor(digitosUser / 10)
+        numeroDigitos += 1
     }
+
+    respuestaDigitos.innerHTML = `El número ingresado por el usuario tiene ${numeroDigitos} digitos`
 })
 
 //____________________________________________________________________________________________________
 
 // Ejercicio 7 Tabla de multiplicar: Desarrolla un programa que imprima la tabla de multiplicar de un número ingresado por el usuario utilizando un bucle while.
 
+// Abre ventana
+let btnEjercicio07 = document.getElementById('button7')
+
+btnEjercicio07.addEventListener('click', function() {
+    document.getElementById('float-window-7').classList.remove('inactive')
+});
+
+// Cierra ventana
+let closeBtn7 = document.getElementById('close-btn7')
+let ventanaEjercicio7 = closeBtn7.parentNode
+
+closeBtn7.addEventListener('click', function() {
+    ventanaEjercicio7.classList.add('inactive')
+})
+
+let inputNumeroTabla = document.getElementById('numero-tabla')
+let crearTabla = document.getElementById('crearTabla')
+let muestraTabla = document.getElementById('muestraTabla')
+
+crearTabla.addEventListener('click', function(event) {
+    event.preventDefault()
+    muestraTabla.textContent = ''
+    let numeroTabla = inputNumeroTabla.value
+    let i = 0
+
+    while(i <= 10) {
+        muestraTabla.innerHTML += `<p>${numeroTabla} * ${i} = ${numeroTabla * i}</p>`
+        i++
+    }
+})
+
 //____________________________________________________________________________________________________
 
 // Ejercicio 8 Conversor de temperatura: Implementa un programa que convierta una temperatura en grados Celsius a grados Fahrenheit utilizando un bucle while para permitir al usuario realizar múltiples conversiones.
+
+// Abre ventana
+let btnEjercicio08 = document.getElementById('button8')
+
+btnEjercicio08.addEventListener('click', function() {
+    document.getElementById('float-window-8').classList.remove('inactive')
+});
+
+// Cierra ventana
+let closeBtn8 = document.getElementById('close-btn8')
+let ventanaEjercicio8 = closeBtn8.parentNode
+
+closeBtn8.addEventListener('click', function() {
+    ventanaEjercicio8.classList.add('inactive')
+})
+
+let btnCicloGrados = document.getElementById('ciclo-grados')
+let muestraConversion = document.getElementById(`muestraConversion`)
+
+btnCicloGrados.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    let celciusUser = 1
+    while(celciusUser) {
+        celciusUser = Number(prompt(`Cuantos grados celcius quieres convertir a Fahrenheit?:`))
+        muestraConversion.innerHTML += `<p>${celciusUser} grados celcius equivalen a ${(celciusUser * (9/5)) + 32 } grados fahrenheit`
+    }
+})
 
 //____________________________________________________________________________________________________
 
 // Ejercicio 9 Detección de números primos: Escribe un programa que determine si un número ingresado por el usuario es primo o no utilizando un bucle while.
 
+// Abre ventana
+let btnEjercicio09 = document.getElementById('button9')
+
+btnEjercicio09.addEventListener('click', function() {
+    document.getElementById('float-window-9').classList.remove('inactive')
+});
+
+// Cierra ventana
+let closeBtn9 = document.getElementById('close-btn9')
+let ventanaEjercicio9 = closeBtn9.parentNode
+
+closeBtn9.addEventListener('click', function() {
+    ventanaEjercicio9.classList.add('inactive')
+})
+
+let inputUserPrimo = document.getElementById(`userPrimo`)
+let btnValidarPrimo = document.getElementById('validarPrimo')
+let muestraPrimo = document.getElementById(`muestraPrimo`)
+
+btnValidarPrimo.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    let number = inputUserPrimo.value
+    let i = 2
+    let noEsPrimo = 0
+    while(i < number) {
+        if (number == 2) {
+            noEsPrimo = 0
+        } else if (number % i == 0) {
+            noEsPrimo++
+        }
+        i++
+    }
+    if (noEsPrimo > 0) {
+        muestraPrimo.textContent = `${number} no es un número primo`
+        } else {
+            muestraPrimo.textContent = `${number} es un número primo`
+            }
+})
+
 //____________________________________________________________________________________________________
 
 // Ejercicio 10 Simulador de ahorro: Crea un programa que simule un plan de ahorro, donde el usuario ingresa un monto de dinero a ahorrar mensualmente y una tasa de interés. Utiliza un bucle while para calcular el saldo acumulado después de un número de meses especificado por el usuario.
+
+// Abre ventana
+let btnEjercicio10 = document.getElementById('button10')
+
+btnEjercicio10.addEventListener('click', function() {
+    document.getElementById('float-window-10').classList.remove('inactive')
+});
+
+// Cierra ventana
+let closeBtn10 = document.getElementById('close-btn10')
+let ventanaEjercicio10 = closeBtn10.parentNode
+
+closeBtn10.addEventListener('click', function() {
+    ventanaEjercicio10.classList.add('inactive')
+})
+
+let inputAhorroMes = document.getElementById('ahorro-mes')
+let mesesAhorrados = document.getElementById('meses-ahorrados')
+let calcAhorro = document.getElementById('calcAhorro')
+let muestraAhorro = document.getElementById('muestraAhorro')
+
+calcAhorro.addEventListener('click', function calcularAhorros(event) {
+    event.preventDefault()
+
+    let ahorroMensual = parseFloat(inputAhorroMes.value)
+    console.log(ahorroMensual)
+    let meses = parseInt(mesesAhorrados.value)
+    console.log(meses)
+    let tasaInteresMes = 0.10 / 12
+
+    let total = 0
+    let interesMes = 0
+    let i = 0
+    
+    while (i < meses) {
+        interesMes = (total + ahorroMensual) * tasaInteresMes
+        total += ahorroMensual + interesMes
+        i++
+    }
+    console.log(total.toFixed(2))
+    muestraAhorro.innerHTML = `Total ahorrado después de ${meses} meses es: $${total.toFixed(2)}`;
+}
+)
